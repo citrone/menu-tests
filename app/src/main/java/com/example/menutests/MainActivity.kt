ca9b31ad67c4ab99ca0f798062902b6febf8a811
Gabriel Cirtea <gcirtea@yahoo.com>
2019-11-04 12:50:22 +0200

    Add categories menu item and handle clicks on it

--- app/src/main/java/com/example/menutests/MainActivity.kt
+++ app/src/main/java/com/example/menutests/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.menutests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set action bar
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_categories -> {
+            Toast.makeText(this, "You clicked on the menu button", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
+
